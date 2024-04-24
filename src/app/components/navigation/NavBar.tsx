1a6f76912b28cac03991716bf7fa0d6945d41c16
jmfp <therealjmfp@gmail.com>
2024-04-24 18:36:16 -0400

    started adding admin ui

---
+++ src/app/components/navigation/NavBar.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import { PathnameContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps, ReactNode } from "react";
+
+export function NavBar({children}: {children: ReactNode}){
+    return(
+        <nav className="flex justify-center px-6"></nav>
+    )
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "classname">){
+    const pathName = usePathname()
+    return(
+        <Link {...props} className={cn("p-4", pathName === props.href && "bg-background text-foreground")}/>
+    )
+}
